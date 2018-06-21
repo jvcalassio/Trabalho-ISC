aba92586cba14ab39896b1d3c5f7a34c6c892152
jvcalassio <39389995+jvcalassio@users.noreply.github.com>
2018-06-21 12:32:18 -0300

    Adicionado sprite desenhado no mapa

--- jogo.s
+++ jogo.s
@@ -4,11 +4,13 @@
 # s1 = qtd vidas
 # s2 = score
 # s3 = high score
+# s4 = pos x do boneco
 
 .data
 # recursos externos
 FUNDO: .string "sprites/map_sp.bin"
-BONECO: .string "sprites/banana.bin"
+BONECO: .string "sprites/b_med_open.bin"
+BONECO_SP: .space 225
 
 #strings do jogo
 titulo: .string "MS PACMAN"
@@ -22,14 +24,16 @@ pbotao: .string "PRESSIONE QUALQUER BOTAO PARA JOGAR"
  	la t0,exceptionHandling		# carrega em t0 o endereco base das rotinas do sistema ECALL
  	csrrw zero,5,t0 		# seta utvec (reg 5) para o endere?o t0
  	csrrsi zero,0,1 		# seta o bit de habilitacao de interrupcao em ustatus (reg 0)
-	
-	#seta os valores iniciais:
-	# t6 = 0, pra checar o botao no menu
-	# nivel 1, 3 vidas, 0 score, 0 high score
-	add t6, zero,zero
-	addi s0, zero, 1
-	addi s1, zero, 3
-	addi s4, zero, 110
+
+#----------------------------------------------
+# Seta os valores iniciais. Utilizar caso dê game over
+#----------------------------------------------
+RESET:
+	add t6, zero,zero # seta t6 = 0 (tecla pressionada)
+	addi s0, zero, 1 # seta s0 = 1 (mapa atual, no caso, o primeiro mapa)
+	addi s1, zero, 3 # seta s1 = 0 (qtd de vidas)
+	addi s4, zero, 100 # seta s4 = 110 (posicao x inicial do pac man)
+	addi s5, zero, 180 # seta s5 = 180 (posicao y inicial do pac man)
 #----------------------------------------------
 # Menu principal do jogo
 # Qualquer tecla inicia
@@ -37,18 +41,18 @@ pbotao: .string "PRESSIONE QUALQUER BOTAO PARA JOGAR"
 
 STATIC_MENU: # parte estatica do menu
 	# printa as vidas
-	mv a0,s1
-	li a1,38
-	li a2,10
-	li a3,0x00ff
-	li a7,101
+	mv a0,s1 # coloca a qtd de vidas em a0
+	li a1,38 # posicao x do texto
+	li a2,10 # posicao y do texto
+	li a3,0x00ff # 00 = cor de fundo; ff = cor da letra
+	li a7,101 # chama o syscall 101, de print int do SYSTEMv11.s
 	ecall
 	
-	la a0,up
-	li a1,56
-	li a2,10
-	li a3,0x00ff
-	li a7,104
+	la a0,up # coloca o texto UP em a0
+	li a1,56 # posicao x do texto
+	li a2,10 # posicao y do texto
+	li a3,0x00ff # 00 = cor de fundo; ff = cor da letra
+	li a7,104 # chama o syscall 104, de print string do SYSTEMv11.s
 	ecall
 	
 	# printa o high score
@@ -99,7 +103,7 @@ STATIC_MENU: # parte estatica do menu
 DYN_MENU: # parte dinamica do menu
 	jal TECLADO	# checa se o user apertou algum botao
 	bne t6,zero,BACKGROUND # se t6 for diferente de zero, executa o jogo, caso contrario, continua no loop
-	j DYN_MENU
+	j DYN_MENU # cria o loop para sempre esperar alguma tecla ser pressionada
 	
 #----------------------------------------------
 # Execucao do jogo
@@ -118,7 +122,56 @@ BACKGROUND:  #background do jogo
 	
 	li a7,57 # syscall de close file
 	ecall
-
+	jal SPAWN
+	
+	
+SPAWN: # spawna o pac man pela primeira vez
+	la a0,BONECO 
+	li a1,0
+	li a7,1024
+	ecall # carrega o pac man em a0 (open)
+	
+	mv t0,a0
+	la a1,BONECO_SP
+	li a2,225
+	li a7,63
+	ecall # usa a0 para abrir a img 
+	
+	mv a0,t0
+	li a7,57
+	ecall # fecha o arquivo
+	
+	jal SPAWN2 # chama a funcao para gerar o pac man corretamente
+	jr ra,0 # retorna
+	
+#----------------------------------------------
+# Funcao que gera objetos no mapa
+# To-do-List:
+#   Adaptar para servir para qualquer objeto, bastando setar a posicao anteriormente e qual o sprite a ser utilizado
+# Por enquanto, só funciona com o primeiro pac man
+#----------------------------------------------
+SPAWN2:
+	li t0,15 # altura do objeto
+	li t1,15 # largura do objeto
+	li a1,0xff000000 # endereco inicial para desenhar o obj
+	la a2,BONECO_SP # espaco
+	SPAWN2_LOOP: beqz t0,SPAWN2_LOOP2 # caso t0 seja 0, pula para a prox linha
+		lb t3,0(a2) # caso contrario, decrementa t0 e repete, até terminar os 15px horizontais da imagem.
+		sb t3,0(a1)
+		addi t0,t0,-1 # decrementa t0
+		addi a2,a2,1 # aumenta a2 (tamanho do espaço BONECO_SP)
+		addi a1,a1,1 # aumenta a1 (endereco a ser desenhado)
+		j SPAWN2_LOOP
+		
+ 		SPAWN2_LOOP2:beqz t1,FIM_SPAWN2 # caso esteja na ultima linha, para a execucao do loop
+			addi t1,t1,-1 # caso contrario, pula uma linha e dá o espacamento necessario até chegar na posicao necessaria
+			addi a1,a1,305
+			addi t0,t0,15
+			j SPAWN2_LOOP # volta para o primeiro loop, para desenhar a proxima linha
+			
+			FIM_SPAWN2:
+				jr ra,0 # retorna
+				
 #----------------------------------------------
 # Finaliza a execucao
 #----------------------------------------------
@@ -131,11 +184,13 @@ FIM:
 #----------------------------------------------
 TECLADO:
 	li t2,0xff200000 # endereco do MIMO
-	lw t3,0(t2) #salva a tecla em t0
-	beq t3,zero,RETORNA
+	lw t3,0(t2) # coloca a tecla em t3
+	beq t3,zero,RETORNA # se nao houver tecla pressionada, volta
 	lw t6,4(t2) # le a tecla pressionada em t6
 # Retorna para o antigo PC
 RETORNA: jr ra,0	
 
+CALC_POS: # calcula as posicoes, de px para o endereco desejado
+	# a ser implementado
+
 .include "assets/SYSTEMv11.s"
-#180x100 o pacman
